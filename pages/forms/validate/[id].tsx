88a455a80cec5731dea4bb8ac2a631b7ee514d45
Mahdi015 <ferianimahdi93@gmail.com>
2022-08-18 10:28:42 +0100

    🔧 build:

--- pages/forms/validate/[id].tsx
+++ pages/forms/validate/[id].tsx
@@ -45,7 +45,7 @@ function ValidateForm() {
               <input
                 type="text"
                 className="bg-inputbg border outline-none  z-50 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5  border-inputborder placeholder-gray-400 text-white "
-                required
+                required={f.required}
               />
             </div>
           );
